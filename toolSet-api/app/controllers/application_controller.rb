442fc4e5a965ec309f298ffb4f67bf9a6dc3d51f
lanibon <nguyenlan13@gmail.com>
2020-02-18 22:21:42 -0800

    add cookie/auth

--- toolSet-api/app/controllers/application_controller.rb
+++ toolSet-api/app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::API
-   
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+    
+    protect_from_forgery with: :exception
+    before_action :set_csrf_cookie
+    
+    
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+    
+    def current_account
+        User.find_by(id: session[:user_id])
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+    
+    
+        private
+    
+    def set_csrf_cookie
+        cookies["CSRF-TOKEN"] = form_authenticity_token
+    end
 end
