09028e70ff38d87b921df5c88bac2f49b9ae9c7b
lanibon <nguyenlan13@gmail.com>
2020-02-18 22:59:49 -0800

    add namespaced routes

--- toolSet-api/config/routes.rb
+++ toolSet-api/config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    namespace :api do
+        namespace :v1 do
+        
+            # resources :users
+            resources :categories
+            resources :topics
+            resources :lessons
+            resources :attempts
+            resources :reactions
+            resources :comments
+            resources :category_topics
+          
+        
+            get "/auth" => 'sessions#auth'
+            get "/signup" => "users#new", as: "signup"
+            post "/signup" => "users#create"
+            get "/login" => "sessions#new", as: "login"
+            post "/login" => "sessions#create"
+            delete "/logout" => "sessions#destroy"
+
+        end
+    end
 end
