dea557e536a058f58d0de099fea7aa1cd5985dea
lanibon <nguyenlan13@gmail.com>
2020-02-18 22:59:04 -0800

    enable sessions

--- toolSet-api/app/controllers/api/v1/sessions_controller.rb
+++ toolSet-api/app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,25 @@
 class Api::V1::SessionsController < ApplicationController
 
-    
+    def new
+
+    end
+
+    def auth
+        render json: {csrf_auth_token: form_authenticity_token}
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            log_in(user)
+            render json: user, status: 200
+        else
+            render json: { message: "Login credentials were incorrect, please try again.", error: true }
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        render json: {status: 200}
+    end
 end
