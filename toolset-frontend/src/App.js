62697e171fd3defa4db42a45e16d73132187afe7
lanibon <nguyenlan13@gmail.com>
2020-02-23 19:03:06 -0800

    wire up redux and router

--- toolset-frontend/src/App.js
+++ toolset-frontend/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from './components/Layout/Layout'
-
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Category from "./components/Category/Category";
 import './App.css';
 
 function App() {
   return (
-    <div>
-        <Layout>
-
-        </Layout>
-    </div>
+    <Router>
+        <div className="App">
+            <Layout>
+                <Navbar/>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/categories" component={Category} />
+                    <Route path="/about" component={About} />
+                </Switch>
+            </Layout>
+        </div>
+    </Router>
   );
 }
 
