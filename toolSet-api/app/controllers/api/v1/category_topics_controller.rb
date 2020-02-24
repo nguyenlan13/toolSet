fc492fc777a2eeda0d014ae42c0caea06b529bdf
lanibon <nguyenlan13@gmail.com>
2020-02-23 18:58:46 -0800

    add join table controller

---
+++ toolSet-api/app/controllers/api/v1/category_topics_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::CategoryTopicsController < ApplicationController
+
+
+
+    
+end
---
+++ toolSet-api/test/controllers/category_topics_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CategoryTopicsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
