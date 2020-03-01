782b1e91d437c67a5bd589af3d7048b08964d8f0
lanibon <nguyenlan13@gmail.com>
2020-02-29 20:50:37 -0800

    login form

--- toolset-frontend/src/components/Login/LoginForm.js
+++ toolset-frontend/src/components/Login/LoginForm.js
@@ -1,11 +1,53 @@
-import React from "react";
+// import React from "react";
 
-function Login() {
+// function Login() {
+//   return (
+//     <div>
+//       <h1>Login:</h1>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+import React, { useState } from "react";
+
+const LoginForm = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+//   const user = {
+//       user: {
+//           email, password
+//       }
+//   }
+  const handleSubmit = event => {
+    event.preventDefault();
+    // send the inputs to the login thing
+    console.log(email, password);
+    setEmail("");
+    setPassword("");
+
+//    props.handleSubmit(email, password)
+    props.handleSubmit(email, password)
+  };
   return (
-    <div>
-      <h1>Login:</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        className="mr-sm-2"
+        type="text"
+        placeholder="Email"
+        onChange={event => setEmail(event.target.value)}
+        value={email}
+      />
+      <input
+        className="mr-sm-2"
+        type="password"
+        placeholder="Password"
+        onChange={event => setPassword(event.target.value)}
+        value={password}
+      />
+      <input type="submit" />
+    </form> 
   );
-}
-
-export default Login;
+};
+export default LoginForm;
