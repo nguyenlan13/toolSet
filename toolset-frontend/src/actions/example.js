c64786ae1905debf498bf8fea6e420458156c862
lanibon <nguyenlan13@gmail.com>
2020-02-29 20:57:31 -0800

    add example

---
+++ toolset-frontend/src/actions/example.js
@@ -0,0 +1,13 @@
+async function example() {
+    try{
+        const res = await fetch("http://localhost:3001/api/v1/auth", {
+            credentials: 'include'
+        })
+        const tokenJson = await res.json()
+        let csrf_token = tokenJson.csrf_auth_token
+        console.log(tokenJson)
+
+    }catch(error){
+        console.log(error)
+    }
+}
