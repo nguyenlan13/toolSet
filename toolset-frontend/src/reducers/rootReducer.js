18e489fc4783d4d5fa14e5dddce82a38699d51fa
lanibon <nguyenlan13@gmail.com>
2020-02-29 20:54:31 -0800

    add user and category reducers

--- toolset-frontend/src/reducers/rootReducer.js
+++ toolset-frontend/src/reducers/rootReducer.js
@@ -1,7 +1,14 @@
+import { combineReducers } from 'redux'
+import tokenReducer from './tokenReducer'
+import categoryReducer from './categoryReducer'
+import userReducer from './userReducer'
 
 
-const rootReducer = () => {
 
-}
+const rootReducer = (combineReducers) ({
+    csrf_token: tokenReducer,
+    user: userReducer,
+    category: categoryReducer
+})
 
 export default rootReducer
