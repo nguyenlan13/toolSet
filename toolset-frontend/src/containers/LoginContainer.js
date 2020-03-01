cdc96da56d7aef296dbccafa9a185fd7918d6f12
lanibon <nguyenlan13@gmail.com>
2020-02-29 20:53:40 -0800

    login call works

---
+++ toolset-frontend/src/containers/LoginContainer.js
@@ -0,0 +1,50 @@
+import React, {Component } from 'react';
+import { connect } from 'react-redux';
+import LoginForm from '../components/Login/LoginForm';
+import { getToken } from '../actions/authSetup'
+import { login } from '../actions/user'
+
+// function LoginContainer() {
+//   return (
+//     <div>
+//       <h1>Login:</h1>
+//       < LoginForm/>
+//     </div>
+//   );
+// }
+
+// export default LoginContainer;
+
+class LoginContainer extends Component {
+
+    async componentDidMount(){
+        await this.props.get_token()
+    }
+    
+    submitHandler = async (email, password) => {
+        await this.props.login(this.props.csrf_token, email, password)
+    }
+    
+    render(){
+        return(
+            <div>
+            <h1>Login:</h1>
+           <LoginForm handleSubmit={this.submitHandler}
+            />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = ({csrf_token}) => ({
+    csrf_token
+})
+
+
+const mapDispatchToProps = dispatch => ({
+    // get_csrf_token: (csrf_token) => dispatch(dispatch => dispatch({type: GET_CSRF_TOKEN, payload: csrf_token})),
+    get_token: (csrf_token) => dispatch(getToken()),
+    login: (csrf_token, email, password) => dispatch(login(csrf_token, email, password))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer)
