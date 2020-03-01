da9497e881791c2bcf83ecdb516c69b646468fd7
lanibon <nguyenlan13@gmail.com>
2020-02-29 20:51:58 -0800

    add signup form

--- toolset-frontend/src/components/Signup/SignupForm.js
+++ toolset-frontend/src/components/Signup/SignupForm.js
@@ -1,12 +1,66 @@
-import React from "react";
+// import React from "react";
 
-function Signup() {
+// function Signup() {
+//   return (
+//     <div>
+//       <h1>Sign Up!</h1>
+//       <p>Let's add some tools to your tool set!</p>
+//     </div>
+//   );
+// }
+
+// export default Signup;
+
+import React, { useState } from "react";
+
+const SignupForm = (props) => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    // send the inputs to the signup thing
+    console.log(email, username, name, password);
+    setEmail("");
+    setUsername("");
+    setName("");
+    setPassword("");
+
+   props.handleSubmit(email, username, name, password)
+  };
   return (
-    <div>
-      <h1>Sign Up!</h1>
-      <p>Let's add some tools to your tool set!</p>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        className="mr-sm-2"
+        type="text"
+        placeholder="Email"
+        onChange={e => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        className="mr-sm-2"
+        type="text"
+        placeholder="Username"
+        onChange={e => setUsername(e.target.value)}
+        value={username}
+      />
+      <input
+        className="mr-sm-2"
+        type="text"
+        placeholder="Name"
+        onChange={e => setName(e.target.value)}
+        value={name}
+      />
+      <input
+        className="mr-sm-2"
+        type="password"
+        placeholder="Password"
+        onChange={e => setPassword(e.target.value)}
+        value={password}
+      />
+      <input type="submit" />
+    </form> 
   );
-}
-
-export default Signup;
+};
+export default SignupForm;
