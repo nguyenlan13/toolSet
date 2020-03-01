2fe1666755666d8c150829a0bcf36066a7613e20
lanibon <nguyenlan13@gmail.com>
2020-02-29 21:00:56 -0800

    profile file

--- toolset-frontend/src/components/Profile/Profile.js
+++ toolset-frontend/src/components/Profile/Profile.js
@@ -1,11 +1,11 @@
-import React from "react";
+// import React from "react";
 
-function Profile() {
-  return (
-    <div>
-      <h1>Your Profile:</h1>
-    </div>
-  );
-}
+// function Profile() {
+//   return (
+//     <div>
+//       <h1>Your Profile:</h1>
+//     </div>
+//   );
+// }
 
-export default Profile;
+// export default Profile;
