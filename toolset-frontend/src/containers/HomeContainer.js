27095ae4149cbcd93418e1d0bd2825fbfcc29a10
lanibon <nguyenlan13@gmail.com>
2020-02-29 20:50:16 -0800

    add container

---
+++ toolset-frontend/src/containers/HomeContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeContainer() {
+  return (
+    <div>
+      <h1>Welcome to toolSet</h1>
+    </div>
+  );
+}
+
+export default HomeContainer;
---
+++ toolset-frontend/src/containers/ProfileContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfileContainer() {
+  return (
+    <div>
+      <h1>Your Profile:</h1>
+    </div>
+  );
+}
+
+export default ProfileContainer;
