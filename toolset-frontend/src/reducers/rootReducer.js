8ee5424aa89802e3809c9ad4d2efc5bd63d7f650
lanibon <nguyenlan13@gmail.com>
2020-03-02 22:34:56 -0800

    add topic reducer

--- toolset-frontend/src/reducers/rootReducer.js
+++ toolset-frontend/src/reducers/rootReducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux'
 import tokenReducer from './tokenReducer'
 import categoryReducer from './categoryReducer'
 import userReducer from './userReducer'
-
+import topicReducer from './topicReducer'
 
 
 const rootReducer = (combineReducers) ({
     csrf_token: tokenReducer,
     user: userReducer,
-    category: categoryReducer
+    category: categoryReducer,
+    topic: topicReducer
+
 })
 
 export default rootReducer
---
+++ toolset-frontend/src/reducers/topicReducer.js
@@ -0,0 +1,17 @@
+import { 
+    GET_TOPICS,
+    ADD_TOPIC,
+    // GET_TOPIC_LESSONS
+} from '../actionTypes'
+
+export default function topicsReducer(state=[], action){
+
+    switch(action.type){
+        case GET_TOPICS:
+            return action.payload
+        case ADD_TOPIC:
+            return [...state, action.payload]
+        default:
+            return state
+    }
+}
