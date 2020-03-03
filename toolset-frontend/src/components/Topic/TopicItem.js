4e7111aaccec0713c0870e69d49496942ef544a0
lanibon <nguyenlan13@gmail.com>
2020-03-02 22:53:45 -0800

    add topicitem

---
+++ toolset-frontend/src/components/Topic/TopicItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+// import { 
+//     BrowserRouter as Router,
+//     Link,
+//     Route, 
+//     Switch,
+//     useRouteMatch
+// } from 'react-router-dom'
+
+
+const topicItem = ({name, id}) => {
+    return(
+        <div>
+            <p>
+                <Link to={`topics/${id}/lessons`}>
+                {name}
+                </Link>
+            </p>
+        </div>
+    )
+}
+
+export default topicItem
