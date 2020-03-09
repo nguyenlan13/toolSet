f7c7ada14a17068d559b6c92694ba1b8cf3bd58a
lanibon <nguyenlan13@gmail.com>
2020-03-09 05:34:28 -0700

    add lesson and attempt reducers

--- toolset-frontend/src/reducers/rootReducer.js
+++ toolset-frontend/src/reducers/rootReducer.js
@@ -3,13 +3,16 @@ import tokenReducer from './tokenReducer'
 import categoryReducer from './categoryReducer'
 import userReducer from './userReducer'
 import topicReducer from './topicReducer'
-
+import lessonReducer from './lessonReducer'
+import attemptReducer from './attemptReducer'
 
 const rootReducer = (combineReducers) ({
     csrf_token: tokenReducer,
     user: userReducer,
     category: categoryReducer,
-    topic: topicReducer
+    topic: topicReducer,
+    lesson: lessonReducer,
+    attempt: attemptReducer
 
 })
 
