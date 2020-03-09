9cac5b43fa23b1828eae06e634ff79af46521b1b
lanibon <nguyenlan13@gmail.com>
2020-03-09 05:34:06 -0700

    add single lesson details

--- toolset-frontend/src/components/Lesson/LessonItem.js
+++ toolset-frontend/src/components/Lesson/LessonItem.js
@@ -1,24 +1,70 @@
-import React from "react";
-import { Link, Redirect } from 'react-router-dom'
-// import { 
-//     BrowserRouter as Router,
-//     Link,
-//     Route, 
-//     Switch,
-//     useRouteMatch
-// } from 'react-router-dom'
+// import React from "react";
+// import { Link } from 'react-router-dom'
+
+// const lessonItem = ({description, id}) => {
+
+//     return(
+//         <div>
+//             <p>
+//                 <Link to={`/lessons/${id}/attempts`}>
+//                 {description}
+//                 </Link>
+//             </p>
+//         </div>
+//     )
+// }
+
+// export default lessonItem
 
 
-const lessonItem = ({description, id}) => {
-    return(
-        <div>
-            <p>
-                <Link to={`/lessons/${id}/attempts`}>
-                {description}
-                </Link>
-            </p>
-        </div>
-    )
+import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+// import { getLessonAttempts } from '../../actions/lesson'
+// import  AttemptItem  from '../../components/Attempt/AttemptItem'
+
+
+class LessonItem extends Component {
+
+
+    onClick = (csrf_token, id) => {
+        //  this.props.get_lesson_attempts(csrf_token, id)
+    }
+
+    render(){
+        const {csrf_token, name, id} = this.props
+        // if (this.props.category.topics){
+            return(
+                <div>
+                    <p>
+                        <Link onClick={() => this.onClick(csrf_token, id)} to={`/lessons/${id}/attempts`}>
+                            <ul>{name}</ul>
+                        </Link>
+                        {/* {this.props.attempts.map(attempt=> {
+                            return <AttemptItem 
+                                description={lesson.description} 
+                                key={attempt.id} 
+                                id={attempt.id}
+                                topicId={attempt.lesson_id}
+                      
+                            />
+                        })} */}
+                    </p>
+                </div>
+            )
+        }
+      
+}
+    
+
+const mapStateToProps = (state) => {
+    const { topic, lesson, csrf_token, user} = state;
+    return { topics: topic, lessons: lesson, csrf_token, user}
 }
 
-export default lessonItem
+
+const mapDispatchToProps = dispatch => ({
+    // get_lesson_attempts: (csrf_token, id) => dispatch(getLessonAttempts(csrf_token, id))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(LessonItem)
