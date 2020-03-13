dd1b79300697611f6bc58262bfcc519f9e1251d6
lanibon <nguyenlan13@gmail.com>
2020-03-12 22:13:27 -0700

    add topic lessons to lesson controller

--- toolset-frontend/src/containers/LessonContainer.js
+++ toolset-frontend/src/containers/LessonContainer.js
@@ -5,43 +5,50 @@ import { addLesson } from '../actions/lesson'
 import LessonItem  from '../components/Lesson/LessonItem'
 import LessonForm from '../components/Lesson/LessonForm'
 import { getTopicLessons } from '../actions/topic'
-
+import { getLessons } from '../actions/lesson'
 
 class LessonContainer extends Component {
 
-    state = {
-        topicId: null
-    }
+    // state = {
+    //     topicId: null
+    // }
+
+    // setState() {
+    //         topicId: this.props.match.params.topicId
+    //     }
+    // async getLessons() {
+    //     await this.props.get_topic_lessons()
+    // }
 
     componentDidMount(){
-        // this.props.get_topic_lessons()
-        this.setState({
-            topicId: this.props.match.params.id
-        })
-        // console.log(this.props)
+        let topicId = this.props.match.params.topicId
+        // this.props.get_topic_lessons(this.csrf_token, topicId)
+        this.props.get_lessons()
+      
     }
 
 
-    submitHandler = async (description, topicId, userId) => {
-        await this.props.add_lesson(this.props.csrf_token, description, topicId, userId)
+    submitHandler = async (description) => {
+        await this.props.add_lesson(this.props.csrf_token, description, this.props.topic, this.props.user)
     }
 
     render(){
+        console.log(this.props)
         const { lessons } = this.props
+        console.log({lessons})
             return(
                 <div>
                     <h1>Lessons:</h1>
                     {/* <TopicLessons> */}
                     < LessonForm handleSubmit={this.submitHandler}/>
-                        {lessons.map(lesson => {
-                        // {(lesson => { 
-                        
+                        {lessons.map(lesson => {                       
                             return <LessonItem 
                                 description={lesson.description} 
                                 key={lesson.id} 
                                 lessonId={lesson.id}
                                 userId={lesson.user_id}
                                 topicId={lesson.topic_id}
+                                lessonAttempts={lesson.attempts}
                             />
                         })}
 
@@ -59,6 +66,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = dispatch => ({
+    get_lessons: () => dispatch(getLessons()),
     add_lesson: (csrf_token, description, topicId, userId) => dispatch(addLesson(csrf_token, description, topicId, userId)),
     get_topic_lessons: (csrf_token, topicId) => dispatch(getTopicLessons(csrf_token, topicId))
 })
