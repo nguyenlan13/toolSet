d47e3ca623c44948645788964decfdd15ad11a51
lanibon <nguyenlan13@gmail.com>
2020-03-14 01:43:33 -0700

    remove console.logs

--- toolset-frontend/src/components/Signup/SignupForm.js
+++ toolset-frontend/src/components/Signup/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 const SignupForm = (props) => {
-    console.log(props)
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [name, setName] = useState("");
