5f6496bda707cea592178b4775d39b735406d2b8
lanibon <nguyenlan13@gmail.com>
2020-03-18 00:58:59 -0700

    add about and css

--- toolset-frontend/src/components/About/About.js
+++ toolset-frontend/src/components/About/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
   return (
-    <div className="page">
+    <div className="about-page">
       <h1 className="headlines">ABOUT</h1>
       <p className="link-color">toolSet utilizes a learning by teaching methodology to learn new concepts. The concepts can be anything the user chooses. The user creates a lesson and each attempt at the lesson is posted to the community for feedback.  The user can make as many attempts as needed in order to fully understand. </p>
     </div>
