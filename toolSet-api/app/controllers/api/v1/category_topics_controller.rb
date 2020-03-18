a0e81fe80ae055d2e56601458d7334c9c3c03aba
lanibon <nguyenlan13@gmail.com>
2020-03-18 00:54:54 -0700

    add topic to categories

--- toolSet-api/app/controllers/api/v1/category_topics_controller.rb
+++ toolSet-api/app/controllers/api/v1/category_topics_controller.rb
@@ -1,23 +1,49 @@
 class Api::V1::CategoryTopicsController < ApplicationController
 
+    def index
+        category_topics = CategoryTopic.all
+
+        render json: category_topics
+    end
+
     def create
         if params[:category_id]
             category = Category.find(params[:category_id])
             topic = Topic.create(name: params[:name])
-            category_topic = CategoryTopic.new(category: Category.find(params[:category_id]), topic: topic)
-            if category_topic.save
-                render json: { message: "saved!", error: false, category_topic: category_topic}
-            else
-                render json: { message: "Sorry, was not saved. Please try again.", error: true }
-            end
-        else 
+        else
             category = Category.create(name: params[:name])
-            category_topic = CategoryTopic.new(category: category, topic: Topic.find(params[:topic_id]))
-            if category_topic.save
-                render json: { message: "saved!", error: false, category_topic: category_topic}
-            else
-                render json: { message: "Sorry, was not saved. Please try again.", error: true }
-            end
+            topic = Topic.find(params[:topic_id])
         end
+
+        category_topic = CategoryTopic.new(category: category, topic: topic)
+
+        if category_topic.save
+            render json: category_topic, status: 200
+            # { message: "saved!", error: false, category_topic: category_topic}
+        else
+            render json: { message: "Sorry, was not saved. Please try again.", error: true }
+        end
+
+
+
+        # if params[:category_id]
+        #     category = Category.find(params[:category_id])
+        #     topic = Topic.create(name: params[:name])
+        #     category_topic = CategoryTopic.new(category: category, topic: topic)
+        #     if category_topic.save
+        #         render json: { message: "saved!", error: false, category_topic: category_topic}
+        #     else
+        #         render json: { message: "Sorry, was not saved. Please try again.", error: true }
+        #     end
+        # else 
+        #     category = Category.create(name: params[:name])
+        #     topic = Topic.find(params[:topic_id])
+        #     category_topic = CategoryTopic.new(category: category, topic: topic)
+        #     if category_topic.save
+        #         render json: { message: "saved!", error: false, category_topic: category_topic}
+        #     else
+        #         render json: { message: "Sorry, was not saved. Please try again.", error: true }
+        #     end
+        # end
     end
 end
--- toolSet-api/app/controllers/api/v1/topics_controller.rb
+++ toolSet-api/app/controllers/api/v1/topics_controller.rb
@@ -17,11 +17,16 @@ class Api::V1::TopicsController < ApplicationController
     end
 
     def create
-        topic = Topic.new(topic_params)
-        if topic.save
-            render json: topic
+        if params[:category_id]
+            category = Category.find(params[:category_id])
+            topic = category.topics.build(topic_params)
         else
-            render json: { message: "Sorry, topic could not be created, please try again.", error: true}, status: 400
+            topic = Topic.new(topic_params)
+            if topic.save
+                render json: topic, status: 200
+            else
+                render json: { message: "Sorry, topic could not be created, please try again.", error: true}, status: 400
+            end
         end
     end
 
