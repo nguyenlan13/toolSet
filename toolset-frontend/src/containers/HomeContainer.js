17039e8f351fdc337a8bca6e0bab2b857975a2ae
lanibon <nguyenlan13@gmail.com>
2020-03-18 00:57:07 -0700

    add css

--- toolset-frontend/src/containers/HomeContainer.js
+++ toolset-frontend/src/containers/HomeContainer.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function HomeContainer() {
   return (
-    <div style={{paddingTop: "50px"}}>
+    <div style={{paddingTop: "80px"}}>
       <h1 className="headlines">ADD A NEW TOOL TO YOUR TOOLSET!</h1>
       <div style={{paddingTop: "30px"}}>
         <p style={{fontSize: "x-large"}} className="link-color">Please <Link className="layout-link" to="/login"> LOG IN to begin.</Link> </p> 
