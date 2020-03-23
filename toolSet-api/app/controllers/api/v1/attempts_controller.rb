7d035181c4450fd650e15da6cb7520081de06447
lanibon <nguyenlan13@gmail.com>
2020-03-22 23:19:23 -0700

    fixed, attempts render after creating

--- toolSet-api/app/controllers/api/v1/attempts_controller.rb
+++ toolSet-api/app/controllers/api/v1/attempts_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::AttemptsController < ApplicationController
         else
             attempts = Attempt.all
         end
-        render json: attempts, status: 200
+        render json: attempts, include: [:lesson], status: 200
     end
 
     def create
@@ -16,7 +16,8 @@ class Api::V1::AttemptsController < ApplicationController
         attempt = lesson.attempts.build(attempt_params)
         #authorize(attempt)
         if attempt.save
-            render json: { message: "Attempt successfully saved!", error: false}
+            render json: attempt, status: 200
+            # { message: "Attempt successfully saved!", attempt: attempt, error: false}
         else
             render json: { message: "Sorry, attempt could was not saved. Please try again.", error: true }
         end
