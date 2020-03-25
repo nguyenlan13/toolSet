a686f3a44715effe82104a3e006a632436c3e2f6
lanibon <nguyenlan13@gmail.com>
2020-03-25 00:14:18 -0700

    change container for lessons index

--- toolset-frontend/src/containers/LessonContainer.js
+++ toolset-frontend/src/containers/LessonContainer.js
@@ -1,41 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-// import { getLessons } from '../actions/lesson'
-import { addLesson } from '../actions/lesson'
-import { getTopicLessons } from '../actions/lesson'
+import { getLessons } from '../actions/lesson'
 import LessonItem  from '../components/Lesson/LessonItem'
-import LessonForm from '../components/Lesson/LessonForm'
 
 class LessonContainer extends Component {
 
     componentDidMount(){
-        // this.props.get_lessons()
-        let topicId = this.props.match.params.topicId
-        this.props.get_topic_lessons(this.props.csrf_token, topicId)
+        this.props.get_lessons()
     }
 
-
-    submitHandler = async (description) => {
-        let topicId = this.props.match.params.topicId
-        await this.props.add_lesson(this.props.csrf_token, description, topicId)
-    }
 
     render(){
-        const { topicLessons } = this.props
-        // const topicName = topicLessons.map(lesson => {
-        //     return lesson.topic.name.toUpperCase()
-        // })
-        const sortedTopicLessons = topicLessons.sort(function(a,b){
-            if(a.name < b.name) {return -1;}
-            if(a.name > b.name) {return 1;}
+        const { lessons } = this.props
+        const sortedLessons = lessons.sort(function(a,b){
+            if(a.description < b.description) {return -1;}
+            if(a.description > b.description) {return 1;}
             return 0
         })
         return(
             <div className="page">
-            {/* <h1 className="headlines">{topicName[0]}</h1> */}
             <h1 className="headlines">LESSONS:</h1>
-                <LessonForm handleSubmit={this.submitHandler}/>
-                    {sortedTopicLessons.map(lesson => {                       
+                    {sortedLessons.map(lesson => {                       
                         return <LessonItem 
                             description={lesson.description} 
                             key={lesson.id} 
@@ -55,16 +40,13 @@ const mapStateToProps = (state) => {
     const { lessons, csrf_token, user } = state;
     return { 
         lessons: lessons.lessons, 
-        topicLessons: lessons.topicLessons,
         csrf_token: csrf_token, 
         user: user
     }
 }
 
 const mapDispatchToProps = dispatch => ({
-    // get_lessons: () => dispatch(getLessons()),
-    add_lesson: (csrf_token, description, topicId) => dispatch(addLesson(csrf_token, description, topicId)),
-    get_topic_lessons: (csrf_token, topicId) => dispatch(getTopicLessons(csrf_token, topicId))
+    get_lessons: () => dispatch(getLessons())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(LessonContainer)
