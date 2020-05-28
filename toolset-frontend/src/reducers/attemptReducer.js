e8a34e44a1a8e2087e7680f99707b0ff627e63e7
lanibon <nguyenlan13@gmail.com>
2020-05-27 21:59:13 -0700

    add get reactions

--- toolset-frontend/src/reducers/attemptReducer.js
+++ toolset-frontend/src/reducers/attemptReducer.js
@@ -3,11 +3,13 @@ import {
     GET_ATTEMPTS,
     ADD_ATTEMPT,
     GET_LESSON_ATTEMPTS,
+    GET_ATTEMPT_REACTIONS
 } from '../actionTypes'
 
 export default function attemptReducer(state = {
         attempts: [],
         loading: false,
+        attemptReactions: [],
         lessonAttempts: []
     }, action){
 
@@ -16,6 +18,8 @@ export default function attemptReducer(state = {
             return {...state, loading: true}
         case GET_ATTEMPTS:
             return {...state, attempts: action.payload, loading: true}
+        case GET_ATTEMPT_REACTIONS:
+            return {...state, attemptReactions: action.payload, loading: true}
         case ADD_ATTEMPT:
             // return [...state, action.payload]
             return {...state, lessonAttempts: [...state.lessonAttempts, action.payload]}
