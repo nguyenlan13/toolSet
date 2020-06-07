c5af1d5242fda556e846640f84319e657c683180
lanibon <nguyenlan13@gmail.com>
2020-06-07 12:13:46 -0700

    add delete reaction action

--- toolset-frontend/src/actions/reaction.js
+++ toolset-frontend/src/actions/reaction.js
@@ -31,3 +31,30 @@ export const addReaction = (csrf_token, reaction_type, attemptId) => {
         }
     }
 }
+
+export const deleteReaction = (csrf_token, reaction_type, attemptId) => {
+    return async function (dispatch) {
+        try{
+            let response = await fetch(baseURL + "reactions",{
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': csrf_token
+                },
+                body: JSON.stringify({reaction: {reaction_type, attemptId}}),
+                credentials: 'include'
+            })
+            if(!response.ok){
+                throw response
+            }
+            let reactionJson = await response.json()
+            dispatch({
+                type: DELETE_REACTION,
+                payload: reactionJson
+            })
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+}
